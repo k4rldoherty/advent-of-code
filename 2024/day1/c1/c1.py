495d9e8ec65cff945dde6af5f45a0423c9f92abb
Karl Doherty <karldoherty15@icloud.com>
2024-12-10 18:46:34 +0000

    initial commit

---
+++ 2024/day1/c1/c1.py
@@ -0,0 +1,19 @@
+def main():
+    f = open("../input.txt", "r")
+    left = []
+    right = []
+    diff = 0
+    for line in f:
+        line_split = line.split()
+        left.append(int(line_split[0]))
+        right.append(int(line_split[1]))
+
+    left = sorted(left)
+    right = sorted(right)
+
+    for i in range(len(left)):
+        diff += abs(left[i] - right[i])
+
+    print(diff)
+
+main()
---
+++ 2024/day1/c2/c2.py
@@ -0,0 +1,26 @@
+def main():
+    f = open("../input.txt", "r")
+    ans = 0
+    left = []
+    right_occurances = {}
+    for line in f:
+        line_split = line.split()
+        l = int(line_split[0])
+        r = int(line_split[1])
+        left.append(l)
+        if r in right_occurances:
+            right_occurances[r] += 1
+        else:
+            right_occurances[r] = 1
+
+    for num in left:
+        if num in right_occurances:
+            ans += (num * right_occurances[num])
+        else:
+            continue
+
+    print(ans)
+    
+
+    
+main()
