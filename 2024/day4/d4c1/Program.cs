5d409f580cd07c334195a7dc3040cd1f95d218ff
Karl Doherty <karldoherty15@icloud.com>
2025-02-01 22:25:02 +0000

    Day 4 Challenge 1 Complete

--- 2024/day4/d4c1/Program.cs
+++ 2024/day4/d4c1/Program.cs
@@ -40,19 +40,82 @@ namespace d4c1
 		public static int CountOccurances(List<List<char>> chars)
 		{
 			// for every letter there can be 8 directions up, down, right, left, and diagonal up left ..brute force is to try all of them but theres definetely a better way
-			for (int i = 0; i < chars.Count; i++)
+			var occurances = 0;
+			for (int r = 0; r < chars.Count; r++)
 			{
-				for (int j = 0; j < chars[0].Count; j++)
+				for (int c = 0; c < chars[0].Count; c++)
 				{
-					char currLetter = chars[i][j];
-					if(currLetter.Equals('X'))
-					{
-						Console.WriteLine($"{currLetter}");
-						// Check up, down, left, right, and diagonal ...
-					}
+					char currLetter = chars[r][c];
+					if(!currLetter.Equals('X')) continue;
+					if(r-3 >= 0) occurances += CountUp(chars, r, c);
+					if(r+3 < chars.Count) occurances += CountDown(chars, r, c);
+					if(c-3 >= 0) occurances += CountLeft(chars, r, c);
+					if(c+3 < chars[0].Count) occurances += CountRight(chars, r, c);
+					// Diagonal
+					if(r-3 >= 0 && c-3 >= 0) occurances += CountDiagUpLeft(chars, r, c);
+					if(r-3 >= 0 && c+3 < chars[0].Count) occurances += CountDiagUpRight(chars, r, c);
+					if(r+3 < chars.Count && c+3 < chars[0].Count) occurances += CountDiagDownRight(chars, r, c);
+					if(r+3 < chars.Count && c-3 >= 0) occurances += CountDiagDownLeft(chars, r, c);
 				}
 			}
-			return 0;
+			return occurances;
+		}
+
+		public static int CountUp(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row-1][col].Equals('M')) return 0;
+			if(!chars[row-2][col].Equals('A')) return 0;
+			if(!chars[row-3][col].Equals('S')) return 0;
+			return 1;
+		}
+		public static int CountDown(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row+1][col].Equals('M')) return 0;
+			if(!chars[row+2][col].Equals('A')) return 0;
+			if(!chars[row+3][col].Equals('S')) return 0;
+			return 1;
+		}
+		public static int CountRight(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row][col+1].Equals('M')) return 0;
+			if(!chars[row][col+2].Equals('A')) return 0;
+			if(!chars[row][col+3].Equals('S')) return 0;
+			return 1;
+		}
+		public static int CountLeft(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row][col-1].Equals('M')) return 0;
+			if(!chars[row][col-2].Equals('A')) return 0;
+			if(!chars[row][col-3].Equals('S')) return 0;
+			return 1;
+		}
+		public static int CountDiagUpRight(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row-1][col+1].Equals('M')) return 0;
+			if(!chars[row-2][col+2].Equals('A')) return 0;
+			if(!chars[row-3][col+3].Equals('S')) return 0;
+			return 1;
+		}
+		public static int CountDiagUpLeft(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row-1][col-1].Equals('M')) return 0;
+			if(!chars[row-2][col-2].Equals('A')) return 0;
+			if(!chars[row-3][col-3].Equals('S')) return 0;
+			return 1;
+		}
+		public static int CountDiagDownRight(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row+1][col+1].Equals('M')) return 0;
+			if(!chars[row+2][col+2].Equals('A')) return 0;
+			if(!chars[row+3][col+3].Equals('S')) return 0;
+			return 1;
+		}
+		public static int CountDiagDownLeft(List<List<char>> chars, int row, int col)
+		{
+			if(!chars[row+1][col-1].Equals('M')) return 0;
+			if(!chars[row+2][col-2].Equals('A')) return 0;
+			if(!chars[row+3][col-3].Equals('S')) return 0;
+			return 1;
 		}
 	}
 }
--- 2024/day4/d4c1/obj/Debug/net9.0/d4c1.AssemblyInfo.cs
+++ 2024/day4/d4c1/obj/Debug/net9.0/d4c1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("d4c1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb1408c21ea89db9dc799ecaea713639299bfe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d1d54960b584a28dd6363ff68e0731d2330d1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("d4c1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("d4c1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
