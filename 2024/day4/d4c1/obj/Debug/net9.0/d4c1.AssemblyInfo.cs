53d1d54960b584a28dd6363ff68e0731d2330d1e
Karl Doherty <karldoherty15@icloud.com>
2025-02-01 21:31:28 +0000

    hope this worked

--- 2024/day4/d4c1/obj/Debug/net9.0/d4c1.AssemblyInfo.cs
+++ 2024/day4/d4c1/obj/Debug/net9.0/d4c1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("d4c1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+560984cdbc73365faada35298c8f7db3c3ee8e65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb1408c21ea89db9dc799ecaea713639299bfe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("d4c1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("d4c1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
